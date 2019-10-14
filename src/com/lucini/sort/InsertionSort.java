b1722bd2a61944b34e023df69b7f0d28d9333f40
rafael.lucini <rafael.lucini@viasoft.com.br>
2019-10-14 13:20:12 -0300

    Insertion Sort

---
+++ src/com/lucini/sort/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.lucini.sort;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] intArray = {10, -15, 8, 35, -11, 55, 12};
+
+        for (int primeiroIndexNaoOrdenado = 1; primeiroIndexNaoOrdenado < intArray.length; primeiroIndexNaoOrdenado++) {
+            int novoElemento = intArray[primeiroIndexNaoOrdenado];
+            int i;
+
+            for(i = primeiroIndexNaoOrdenado; i > 0 && intArray[i -1] > novoElemento; i--) {
+                intArray[i] = intArray[i -1];
+            }
+            intArray[i] = novoElemento;
+        }
+
+
+        // IMPRIMIR
+        for (int i =0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
